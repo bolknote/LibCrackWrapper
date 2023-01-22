85fe6595acfe38bf6f13d9211e63255c999c4cb7
Evgeny Stepanischev <Evgeny.Stepanischev@tatar.ru>
2023-01-22 20:29:14 +0300

    Tests added

--- tests/LibCrackWrapper/WrapperTest.php
+++ tests/LibCrackWrapper/WrapperTest.php
@@ -17,12 +17,13 @@ class WrapperTest extends TestCase
      * @covers \LibCrackWrapper\Wrapper::checkPassword
      * @dataProvider checkingPasswordProvider
      */
-    public function testCheckPassword(string $pass, string $message): void
+    public function testCheckPassword(string $pass, string $message, int $code): void
     {
         $result = static::$wrapper->checkPassword($pass);
 
         $this->assertSame($message, (string)$result);
         $this->assertSame($message, $result->getLocalizedMessage());
+        $this->assertSame($code, $result->getCode());
     }
 
     /**
@@ -57,40 +58,41 @@ class WrapperTest extends TestCase
         string $user,
         string $userinfo,
         string $message,
+        int $code,
     ): void
     {
-        if (static::$wrapper->getBackendName() === 'FFI') {
-            $this->assertSame(
-                (string) static::$wrapper->checkUserAndPassword($pass, $user, $userinfo),
-                $message
-            );
-        } else {
+        if (static::$wrapper->getBackendName() !== 'FFI') {
             $this->markTestSkipped();
         }
+
+        $result = static::$wrapper->checkUserAndPassword($pass, $user, $userinfo);
+
+        $this->assertSame((string) $result, $message);
+        $this->assertSame($result->getCode(), $code);
     }
 
     public function checkingkUserAndPasswordProvider(): array
     {
         return [
-            ['username', 'username', '', 'it is based on your username'],
-            ['username', '', 'username', 'it is based upon your password entry'],
-            ['hxuzohtmn', '', 'hxu zoh tmn', 'it is derived from your password entry'],
-            ['EStepanis', '', 'Evgeny Stepanischev', 'it is derivable from your password entry'],
-            ['SEvgen', '', 'Evgeny Stepanischev', "it's derivable from your password entry"],
+            ['username', 'username', '', 'it is based on your username', 1],
+            ['username', '', 'username', 'it is based upon your password entry', 2],
+            ['hxuzohtmn', '', 'hxu zoh tmn', 'it is derived from your password entry', 3],
+            ['EStepanis', '', 'Evgeny Stepanischev', 'it is derivable from your password entry', 4],
+            ['SEvgen', '', 'Evgeny Stepanischev', "it's derivable from your password entry", 5],
         ];
     }
 
     public function checkingPasswordProvider(): array
     {
         return [
-            ['test', 'it is too short'],
-            ['abc',  'it is WAY too short'],
-            ['xxxxxxx', 'it does not contain enough DIFFERENT characters'],
-            ['KPACOTA', 'it is based on a dictionary word'],
-            ['ax134178y', 'it looks like a National Insurance number.'],
-            ["\t\t \x0B\x0C\x0D\t", 'it is all whitespace'],
-            ['01xya12lop23hyr34', 'it is too simplistic/systematic'],
-            [strrev('dinosaur'), 'it is based on a (reversed) dictionary word'],
+            ['abc',  'it is WAY too short', 7],
+            ['test', 'it is too short', 8],
+            ['xxxxxxx', 'it does not contain enough DIFFERENT characters', 9],
+            ["\t\t \x0B\x0C\x0D\t", 'it is all whitespace', 10],
+            ['01xya12lop23hyr34', 'it is too simplistic/systematic', 11],
+            ['ax134178y', 'it looks like a National Insurance number.', 12],
+            ['KPACOTA', 'it is based on a dictionary word', 13],
+            [strrev('dinosaur'), 'it is based on a (reversed) dictionary word', 14],
         ];
     }
 }
